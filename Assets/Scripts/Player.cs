90d858910ec21e31b9fd66ba01cc1651975d5bf8
vrglab <vrglab1@gmail.com>
2020-04-18 14:13:18 +0200

    Added the game it self

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    private ShakeBehivour sb;
+    public ParticleSystem ps;
+    public bool isDead;
+    public float speed = 10f;
+    public GameObject deathPanel;
+    private Rigidbody2D rg2d;
+    private SpriteRenderer sr;
+    public float jumpHeight;
+    private bool isJumping = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sb = GameObject.Find("Main Camera").GetComponent<ShakeBehivour>();
+        rg2d = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) && !isJumping) // both conditions can be in the same branch
+        {
+            rg2d.AddForce(Vector2.up * jumpHeight,ForceMode2D.Impulse); // you need a reference to the RigidBody2D component
+            isJumping = true;
+            print("jumped");
+        }
+
+        rg2d.AddForce(new Vector2(speed, 0), ForceMode2D.Force);
         
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground") // GameObject is a type, gameObject is the property
+        {
+            isJumping = false;
+        }
+    }
+
+
+
+public void Death()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        sr.enabled = false;
+        speed = 0f;
+        if(!ps.isPlaying)
+        {
+            ps.Play();
+        }
+        sb.TriggerShake();
+        deathPanel.SetActive(true);
+        isDead = true;
+    }
+
+    public bool IsPlayerDead()
+    {
+        if(isDead == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void reset()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Spike : MonoBehaviour
+{
+    private Player p;
+
+    void Start()
+    {
+        p = GameObject.Find("Player").GetComponent<Player>();
+      
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            p.Death();
+        }
+    }
+}
